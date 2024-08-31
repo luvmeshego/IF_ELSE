102cdcd94b94879c0d8035e586f396459ee93488
luvmeshego <dimasrex10@gmail.com>
2024-08-31 13:15:19 +0300

    Add files via upload

---
+++ O.py
@@ -0,0 +1,8 @@
+a = int(input()) 
+b = int(input()) 
+c = int(input()) 
+d = int(input())
+if (c + d) - (a + b) == 2 or (c + d) - (a + b) == 1 or (c + d) - (a + b) == -2 or (c + d) - (a + b) == -1:
+    print('YES')
+else:
+    print('NO')
